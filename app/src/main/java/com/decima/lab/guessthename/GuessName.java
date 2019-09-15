ce12560088ca83fc1ef2920e3ae4f22a71bc685b
SHAJ <shakilahmedshaj@gmail.com>
2019-09-15 21:53:49 +0600

    data pass done

--- app/src/main/java/com/decima/lab/guessthename/GuessName.java
+++ app/src/main/java/com/decima/lab/guessthename/GuessName.java
@@ -2,7 +2,9 @@ package com.decima.lab.guessthename;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class GuessName extends AppCompatActivity {
@@ -14,13 +16,28 @@ public class GuessName extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_name);
 
+        Bundle extraData = getIntent().getExtras();
+
         textViewReceived = findViewById(R.id.text_view_received);
 
 
-        if (getIntent().getStringExtra("guess") != null) {
+        if (extraData != null) {
 
-            textViewReceived.setText(getIntent().getStringExtra("guess"));
+            textViewReceived.setText("Hi " + extraData.getString("guess") + "!");
         }
+
+
+        textViewReceived.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = getIntent();
+                intent.putExtra("message_back", "Hi from Guess Activity");
+                setResult(RESULT_OK, intent);
+                finish();
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/decima/lab/guessthename/MainActivity.java
+++ app/src/main/java/com/decima/lab/guessthename/MainActivity.java
@@ -1,5 +1,6 @@
 package com.decima.lab.guessthename;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,11 +9,15 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button showGuess;
     EditText editTextName;
+
+    private final int REQUEST_CODE = 02;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +34,38 @@ public class MainActivity extends AppCompatActivity {
 
                 String name = editTextName.getText().toString().trim();
 
-//                startActivity(new Intent(MainActivity.this, GuessName.class));
 
-                Intent intent = new Intent(MainActivity.this, GuessName.class);
+                if (!name.isEmpty()) {
+                    //                startActivity(new Intent(MainActivity.this, GuessName.class));
 
-                intent.putExtra("guess", name);
+                    Intent intent = new Intent(MainActivity.this, GuessName.class);
 
-                startActivity(intent);
+                    intent.putExtra("guess", name);
+
+                    //startActivity(intent);
+
+                    startActivityForResult(intent, REQUEST_CODE);
+
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Please Enter a Name!", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+
+            assert data != null;
+            String message = data.getStringExtra("message_back");
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
 
 
     }
